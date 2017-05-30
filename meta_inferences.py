5802021e2722061335433177c0016a40953ba985
Sai Kishan Pampana <saikishanpampana@gmail.com>
2017-05-30 22:18:09 +0530

    Change of meta inference rules to make it easier for the search

--- meta_inferences.py
+++ meta_inferences.py
@@ -1,6 +1,8 @@
-
+#In the meta inference, Always use a touple for the inference rules
+# Example (a, (b)) - even if b is the only thing that is needed. This helps
+#in the searching process.
 Inferences = []
-Inferences.append(("Similar",("Angle", "Angle")))
-Inferences.append(("Angle_Angle"), "Intersect"))
+Inferences.append(("Similar",("Angle_Angle", "Angle_Angle")))
+Inferences.append((("Angle_Angle"),( "Intersect_lines")))
 
 
