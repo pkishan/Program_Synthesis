229cb84f60dee99b5e05f21a7d0fc74d4d520edc
Sai Kishan Pampana <saikishanpampana@gmail.com>
2017-05-30 14:13:21 +0530

    Addition of the input file and the inference pool

--- expansion.py
+++ expansion.py
@@ -1,6 +1,8 @@
 import meta_inferences as mi
-
+import input as inp
 C = mi.Inferences
-
+Inference_pool = inp.input_rules 
 for i in C:
+    if i[0] == "Similar" and i[1][0] == "Angle" and i[1][1] == "Angle":
+
 
