20983ed2e2699b2d55ea85256684bc5521d7b54e
Sai Kishan Pampana <saikishanpampana@gmail.com>
2017-05-30 14:12:12 +0530

    The lists containing the sides and angles given by the user

--- input.py
+++ input.py
@@ -1,1 +1,11 @@
 # In this file the user presents his inputs
+
+#These two lists will have the sides of both the triangles.
+sides_one = [("A", "B"), ("B", "C"), ("C", "A")]
+sides_two = [("D", "E"), ("E", "F"), ("F", "A")] 
+
+#These are the tow lists that will have the angles from both tirnalges 
+
+angles_one = [("A", "B", "C"), ("B", "C", "A"), ("C", "A", "B")]
+angles_two = [("D", "E", "F"), ("E", "F", "D"), ("F", "D", "E")]
+
