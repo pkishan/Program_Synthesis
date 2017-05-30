c3deae919579d5bd0644dc28b12b60b51eb48d2a
Sai Kishan Pampana <saikishanpampana@gmail.com>
2017-05-31 02:33:57 +0530

    Modification of input to check the inference rules for intersections

--- input.py
+++ input.py
@@ -2,10 +2,10 @@
 
 #These two lists will have the sides of both the triangles.
 sides_one = [("A", "B"), ("B", "C"), ("C", "A")]
-sides_two = [("D", "E"), ("E", "F"), ("F", "A")] 
+sides_two = [("B", "E"), ("E", "F"), ("F", "B")] 
 
 #These are the tow lists that will have the angles from both tirnalges 
 
 angles_one = [("A", "B", "C"), ("B", "C", "A"), ("C", "A", "B")]
-angles_two = [("D", "E", "F"), ("E", "F", "D"), ("F", "D", "E")]
+angles_two = [("B", "E", "F"), ("E", "F", "B"), ("F", "B", "E")]
 
