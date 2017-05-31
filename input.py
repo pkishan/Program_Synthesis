263e98e371a8ef45c8d6e2f498c4604034d924bf
Sai Kishan Pampana <saikishanpampana@gmail.com>
2017-06-01 03:07:07 +0530

    Modified to implement the case involving parallel lines

--- input.py
+++ input.py
@@ -1,5 +1,12 @@
 # In this file the user presents his inputs
 
+
+#Creating a list that will contain all the input rules that are given by the
+#user
+
+User_Rules = []
+
+
 #These two lists will have the sides of both the triangles.
 sides_one = [("A", "B"), ("B", "C"), ("C", "A")]
 sides_two = [("B", "E"), ("E", "F"), ("F", "B")] 
@@ -9,3 +16,5 @@ sides_two = [("B", "E"), ("E", "F"), ("F", "B")]
 angles_one = [("A", "B", "C"), ("B", "C", "A"), ("C", "A", "B")]
 angles_two = [("B", "E", "F"), ("E", "F", "B"), ("F", "B", "E")]
 
+User_Rules.append((("Parallel", "Across"), (("A", "C"), ("E", "F"), ("C",
+                                                                     "E"))))
