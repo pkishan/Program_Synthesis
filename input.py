08bccc84e5ea978d2225744ed5bbb00cbf6cdb66
Sai Kishan Pampana <saikishanpampana@gmail.com>
2017-06-03 20:06:40 -0700

    Mentioned the conventions that we will employ during inputing the data

--- input.py
+++ input.py
@@ -1,5 +1,14 @@
 # In this file the user presents his inputs
 
+#Some conventions to be followed while giving the inputs
+""" When mentioning a line segment we will align then in the same way as we
+defined in the sides array.
+
+When mentioning an angle we will use the same allignments as the one used in
+the angles array
+
+"""
+#END OF CONVENTIONS
 
 #Creating a list that will contain all the input rules that are given by the
 #user
